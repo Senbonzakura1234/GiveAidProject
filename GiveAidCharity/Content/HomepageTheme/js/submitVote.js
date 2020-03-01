28118822846f415afd9d5444ef9f5215fe630d74
Pham Anh Dung <anhdungpham090@gmail.com>
2020-03-02 03:56:55 +0700

    refine css vote btn, add get rating function ajax, refine submit vote ajax, add interval get vote function.

---
+++ GiveAidCharity/Content/HomepageTheme/js/submitVote.js
@@ -0,0 +1,85 @@
+﻿$(function () {
+    $(".rating-control").on('click', function () {
+        event.preventDefault();
+        var obj = $(this);
+        $("#statusValue").val($(this).hasClass("active") ? 0 : $(this).hasClass("like") ? 1 :
+            $(this).hasClass("dislike") ? -1 : 0);
+        $('#ratingValue').val(function (i, old) {
+            console.log(old);
+            if (obj.hasClass("active")) {
+                if (obj.hasClass("like")) {
+                    old--;
+                } else if (obj.hasClass("dislike")) {
+                    old++;
+                }
+            } else {
+                if (obj.hasClass("like")) {
+                    old++;
+                    if ($(".dislike").hasClass("active")) {
+                        old++;
+                    }
+                } else if (obj.hasClass("dislike")) {
+                    old--;
+                    if ($(".like").hasClass("active")) {
+                        old--;
+                    }
+                }
+            }
+            return old;
+        });
+
+        $(this).toggleClass("active");
+
+        if ($(this).hasClass("like")) {
+            $('.dislike').removeClass("active");
+        } else if ($(this).hasClass("dislike")) {
+            $('.like').removeClass("active");
+        } else {
+            $('.rating-control').removeClass("active");
+        }
+
+        $("#ratingValueDisplay").html($('#ratingValue').val());
+        submitRating();
+    });
+
+    function submitRating() {
+        const form = $('#__AjaxAntiForgeryForm');
+        const token = $('input[name="__RequestVerificationToken"]', form).val();
+        $.ajax({
+            url: $("#urlSubmit").val(),
+            type: 'POST',
+            data: {
+                __RequestVerificationToken: token,
+                userId: $('input[name="userId"]', form).val(),
+                blogId: $('input[name="blogId"]', form).val(),
+                status: $('input[name="status"]', form).val()
+            },
+            success: function (res) {
+                console.log(res);
+            }
+        });
+        setTimeout(
+            function () {
+                getRating1();
+            }, 3000);
+    }
+    function getRating1() {
+        $.ajax({
+            url: $("#urlGetRating").val(),
+            type: 'GET',
+            data: {
+                blogId: $('input[name="blogId"]').val()
+            },
+            success: function (res) {
+                console.log(res.rating);
+                if (res.rating == null) {
+                    $("#ratingValue").val(0);
+                    $("#ratingValueDisplay").html(0);
+                } else {
+                    $("#ratingValue").val(res.rating);
+                    $("#ratingValueDisplay").html(res.rating);
+                }
+            }
+        });
+    }
+});
--- GiveAidCharity/Models/HelperClass/HelperMethod.cs
+++ GiveAidCharity/Models/HelperClass/HelperMethod.cs
@@ -1,9 +1,16 @@
 ﻿using System;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using GiveAidCharity.Models.Main;
 
 namespace GiveAidCharity.Models.HelperClass
 {
     public class HelperMethod
     {
+        private static readonly ApplicationDbContext Db = new ApplicationDbContext();
+
         public static bool CheckValidDate(string date)
         {
             return DateTime.TryParse(date, out _);
@@ -18,5 +25,14 @@ namespace GiveAidCharity.Models.HelperClass
 
             return text;
         }
+
+        internal static async Task<int> RatingCount(string id)
+        {
+            if (id == null) return -(int) HttpStatusCode.BadRequest;
+            var blog = await Db.Blogs.FindAsync(id);
+            if (blog == null) return -(int)HttpStatusCode.NotFound;
+            var votes = await Db.Votes.Where(d => d.Status != Vote.VoteStatusEnum.Neutral && d.BlogId == id).ToListAsync();
+            return votes.Sum(item => (int) item.Status);
+        } 
     }
 }
